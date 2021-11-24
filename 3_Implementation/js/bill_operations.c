899e43945474be7e0d373e713741a32089e84ef6
saravana304priya <60978907+saravana304priya@users.noreply.github.com>
2021-11-24 21:37:45 +0530

    Create bill_operations.c

---
+++ 3_Implementation/js/bill_operations.c
@@ -0,0 +1,17 @@
+#include<bill_operations.c>
+
+void generateBillHeader(char name[5], char date[30]){
+ return name;
+ return date;
+ }
+ 
+ void generateBillBody(char item[30],int qty, float price){
+  return item;
+  return qty;
+  return price;
+  }
+  
+  void generateBillFooter(float total){
+  return nettotal;
+  return discount;
+  }

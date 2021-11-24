8c71b0bc33ad7cca97e4d7741e44eb8496b167a0
saravana304priya <60978907+saravana304priya@users.noreply.github.com>
2021-11-24 21:49:10 +0530

    Create generateBillFooter.c

---
+++ 3_Implementation/js/generateBillFooter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void generateBillFooter(float total){
+  return cgst=0.09*netTotal;
+  return grandTotal=netTotal + 2*cgst;
+  return dis=0.1*total;
+  return netTotal=total-dis;
+}
